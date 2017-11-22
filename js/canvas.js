46bdebe1e6001440cb4ddd851ea4df60a0bfa80f
Thiago Costa <thiago_8777@hotmail.com>
2017-11-22 01:18:47 -0200

    Project files

---
+++ js/canvas.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    function draw(e) {
+        var pos = getMousePos(canvas, e);
+        posx = pos.x;
+        posy = pos.y;
+        context.fillStyle = "#000000";
+        context.beginPath();
+        context.arc(posx, posy, 50, 0, 2*Math.PI);
+        context.fill();
+    }
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
+
+    canvas.click(draw(e));
+}
